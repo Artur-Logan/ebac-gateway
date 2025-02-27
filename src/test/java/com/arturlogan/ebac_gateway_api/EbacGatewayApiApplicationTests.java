912decc27447179e5b95cd74b3e6d87118836dc7
Artur Logan <arturlogan1ferreira@gmail.com>
2025-02-27 14:21:05 -0300

    Primeiro commit

---
+++ src/test/java/com/arturlogan/ebac_gateway_api/EbacGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturlogan.ebac_gateway_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbacGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
